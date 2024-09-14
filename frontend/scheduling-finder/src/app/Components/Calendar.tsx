7dfb381a459af38d4733faa0a76cf5fe43edc0bd
Joe Cardenas <coderjoe05@gmail.com>
2024-09-14 01:23:20 -0400

    students page

---
+++ frontend/scheduling-finder/src/app/Components/Calendar.tsx
@@ -0,0 +1,24 @@
+export default function Calendar({ busyArray }: { busyArray: boolean[][] }) {
+    return (
+        <table>
+            <tr>
+                <td></td>
+                <td>Su</td>
+                <td>Mo</td>
+                <td>Tu</td>
+                <td>We</td>
+                <td>Th</td>
+                <td>Fr</td>
+                <td>Sa</td>
+            </tr>
+            {Array.from(Array(48).keys()).map(hour => (
+                <tr className="h-4">
+                    <td>{Math.floor(hour / 2) + ":" + (hour % 2 == 0 ? "00" : "30")}</td>
+                    {Array.from(Array(7).keys()).map(day => (
+                        <td className={busyArray[day][hour] ? "w-10 bg-red-950" : "w-8 bg-slate-600"}></td>
+                    ))}
+                </tr>
+            ))}
+        </table>
+    )
+}
--- frontend/scheduling-finder/src/app/students/[studentId]/page.tsx
+++ frontend/scheduling-finder/src/app/students/[studentId]/page.tsx
@@ -3,12 +3,12 @@
 import { SessionProvider } from "next-auth/react"
 import { useSession } from "next-auth/react"
 
-import HugeTitle from "../Components/Typography/HugeTitle";
-import ListItem from "../Components/ListItem";
+import HugeTitle from "../../Components/Typography/HugeTitle";
+import ListItem from "../../Components/ListItem";
 import { FormEvent, useState } from "react";
-import Calendar from "../Components/Calendar";
+import Calendar from "../../Components/Calendar";
 
-export default function Account() {
+export default function Account({ params }: { params: { studentId: string } }) {
   const randomBooleanArray = [
     [true, false, true, false, true, true, false, true, false, false, true, true, false, true, true, false, true, false, true, true, false, false, true, false, true, true, false, true, false, true, true, false, true, false, false, true, false, true, true, false, true, true, false, false, true, false, true, true],
     [false, true, false, true, false, false, true, false, true, true, false, false, true, false, false, true, false, true, false, false, true, true, false, true, false, false, true, false, true, false, false, true, false, true, true, false, true, false, false, true, false, false, true, true, false, true, false, false],
@@ -18,60 +18,22 @@ export default function Account() {
     [false, true, false, true, false, false, true, false, true, true, false, false, true, false, false, true, false, true, false, false, true, true, false, true, false, false, true, false, true, false, false, true, false, true, true, false, true, false, false, true, false, false, true, true, false, true, false, false],
     [true, false, true, false, true, true, false, true, false, false, true, true, false, true, true, false, true, false, true, true, false, false, true, false, true, true, false, true, false, true, true, false, true, false, false, true, false, true, true, false, true, true, false, false, true, false, true, true],
   ];
-  const [file, setFile] = useState<File>()
-  const { data: session } = useSession()
-  const needsUploading = true;
-
-  const onSubmit = async function(e: FormEvent) {
-    e.preventDefault()
-    if (!file) return;
-
-    try {
-      const data = new FormData()
-      data.set('file', file)
-  
-      const res = await fetch("api/calendar_import", {
-        method: "PUT",
-        body: data
-      });
-    } catch(e) {
-      console.error(e)
-    }
-  }
-
-  if (!session) { return (<p>loading...</p>)}
 
   return (
       <main className="flex flex-col gap-20 items-start justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <header className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
-          <p className="text-3xl">Welcome,</p>
-          <HugeTitle>{session?.user?.name}.</HugeTitle>
+          <p className="text-3xl">Information for</p>
+          <HugeTitle>{params.studentId}.</HugeTitle>
         </header>
         <div className="flex flex-col gap-5 items-start">
-          {needsUploading ? (
-            <form className="flex flex-col items-start gap-2" onSubmit={onSubmit}>
-              <p className="text-3xl">Schedule:</p>
-              <input
-                type="file"
-                name="file"
-                onChange={(e) => setFile(e.target.files?.[0])}
-              />
-              <input className="p-2 text-3xl cursor-pointer hover:bg-slate-200 hover:text-black rounded-md transition-all duration-200 border-slate-200 border" type="submit" value="Upload" />
-            </form>
-          ): (<p>Uploaded successfully.</p>)}
           <div className="flex flex-col items-stretch gap-2">
-            <p className="text-3xl">Find a study group:</p>
-            <ListItem href="account/classes" title="Search by class" />
-            <ListItem href="account/unknown" title="Search by student" />
-          </div>
-          <div className="flex flex-col items-stretch gap-2">
-            <p className="text-3xl">My study groups:</p>
+            <p className="text-3xl">Study groups:</p>
             <ListItem title="Haolin's Study Group" caption="Tuesdays, Thursdays, 7:00pm" href="/groups/haolin-group"/>
             <ListItem title="Tyler's Study Group" caption="Mondays, Fridays, 6:30pm" />
             <ListItem title="Joe's L33t h4x0rz" caption="Saturdays, 3am" />
           </div>
           <div className="flex flex-col items-stretch gap-2">
-            <p className="text-3xl">My schedule:</p>
+            <p className="text-3xl">Schedule:</p>
             <Calendar busyArray={randomBooleanArray}/>
           </div>
         </div>
